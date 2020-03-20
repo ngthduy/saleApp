0c348ee2cd62a120d8484e9201420765ce688ab8
ngthduy <nguyenduy170898@gamil.com>
2020-03-20 14:05:53 +0700

    config time zone HCM

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Saleapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    # set time zone
+    config.time_zone = 'Asia/Ho_Chi_Minh'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
