7f63e7da82959a858f1b73cf0c258844be627fa4
ngthduy <nguyenduy170898@gmail.com>
2020-03-23 16:57:42 +0700

    Task I: Logging In / H2

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -16,11 +16,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create user" do
-    assert_difference('User.count') do
-      post users_url, params: { user: { name: @user.name, password: 'secret', password_confirmation: 'secret' } }
+    assert_difference("User.count") do
+      post users_url, params: { user: { name: "sam", password: "secret", password_confirmation: "secret" } }
     end
 
-    assert_redirected_to user_url(User.last)
+    assert_redirected_to users_url
   end
 
   test "should show user" do
@@ -34,12 +34,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { name: @user.name, password: 'secret', password_confirmation: 'secret' } }
-    assert_redirected_to user_url(@user)
+    patch user_url(@user), params: { user: { name: @user.name, password: "secret", password_confirmation: "secret" } }
+    assert_redirected_to users_url
   end
 
   test "should destroy user" do
-    assert_difference('User.count', -1) do
+    assert_difference("User.count", -1) do
       delete user_url(@user)
     end
 
